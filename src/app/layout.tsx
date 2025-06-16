5fefb726f9d45b149e92b1f66e9c885eba2ddb47
Eduardo Reichardt <eduardoaugustin31@gmail.com>
2025-06-16 13:05:31 -0300

    Adiciona head.tsx com meta tag de verificação do Google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: 'CA Imóveis',
   description: 'Seu portal de imóveis',
   icons: {
-    icon: '/favicon.ico',
+    icon: '/favicon.ico', // <- Aqui está o favicon
   },
 };
 
