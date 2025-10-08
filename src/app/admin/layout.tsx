448b191de9c867b10bac30f8d8e465f028d712e6
Eduardo Reichardt <eduardoaugustin31@gmail.com>
2025-10-08 11:47:33 -0300

    sgr

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,9 @@
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
-import { authOptions } from "@/lib/auth";
+
 import AdminLayoutClient from './AdminLayoutClient';
+import { authOptions } from "../../lib/auth";
+
 
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
