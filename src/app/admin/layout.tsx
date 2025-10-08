122f649daecc5200515b25706d1bb82399c153b0
Eduardo Reichardt <eduardoaugustin31@gmail.com>
2025-10-08 12:06:19 -0300

    oip

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,13 +1,12 @@
+import { getServerSession } from "@/app/lib/auth";
 import { redirect } from "next/navigation";
-import AdminLayoutClient from './AdminLayoutClient';
-import { getServerSession } from "../lib/auth";
-
+import AdminLayoutClient from "./AdminLayoutClient";
 
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession();
 
   // Redireciona se não estiver logado ou não for admin
-  if (!session || session.user?.role !== "admin") {
+  if (!session || session.user.role !== "admin") {
     redirect("/login");
   }
 
